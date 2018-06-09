f584215136627584ebd4a10b8f9d992836251a8f
Christophe Meyer <christophe.meyer.pro@gmail.com>
2018-06-09 18:29:10 -0400

    add form receive

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
 const express = require('express')
+const bodyParser = require('body-parser')
+const form = require('express-form')
+const field = form.field;
 const path = require('path')
 const PORT = process.env.PORT || 5000
 
@@ -7,4 +10,20 @@ express()
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
   .get('/', (req, res) => res.render('pages/index'))
+  .post('/generate',
+       form(
+         field("tenant_name").trim().required().is(/^[a-z]+$/),
+         field("tenant_address").trim().required().is(/^[a-z]+$/)
+       ),
+       function(req, res) {
+         if (!req.form.isValid) {
+           console.log(req.form.errors);
+         } else {
+           console.log("tenant_name:", req.form.tenant_name);
+           console.log("tenant_address:", req.form.tenant_address);
+
+           res.render('pages/lease.ejs')
+         }
+       }
+  )
   .listen(PORT, () => console.log(`Listening on http://localhost:${ PORT }`))
