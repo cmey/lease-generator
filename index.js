71801abb6dd813fffefeae5fa86d31e3efadb195
Christophe Meyer <christophe.meyer.pro@gmail.com>
2018-07-31 19:28:36 -0400

    Add generation of tenant information doc sheet

--- index.js
+++ index.js
@@ -36,6 +36,7 @@ express()
          field("landlord_address").required()
        ),
        function(req, res) {
+         console.log("route generate_lease:");
          console.log("complete body response:", req.body);
          console.log("complete form:", req.form);
          if (!req.form.isValid) {
@@ -45,6 +46,27 @@ express()
            console.log("tenant_address:", req.form.tenant_address);
            req.form.rent_amount_written = writtenNumber(req.form.rent_amount);
            res.render('pages/lease.ejs', req.form)
+         }
+       }
+  )
+  .post('/generate_information_document',
+       form(
+         field("unit").required(),
+         field("rent_amount").required(),
+         field("landlord_name").required(),
+         field("landlord_email").required(),
+         field("landlord_phone").required()
+       ),
+       function(req, res) {
+         console.log("route generate_information_document:");
+         console.log("complete body response:", req.body);
+         console.log("complete form:", req.form);
+         if (!req.form.isValid) {
+           console.log(req.form.errors);
+         } else {
+           console.log("unit:", req.form.tenant_name);
+           req.form.rent_amount_written = writtenNumber(req.form.rent_amount);
+           res.render('pages/information_document.ejs', req.form)
          }
        }
   )
