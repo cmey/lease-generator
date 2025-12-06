bef34801542edf783607a7049f814a8c2bdfce2c
Christophe Meyer <christophe.meyer.pro@gmail.com>
2025-12-06 18:02:32 +0100

    upgrade more, breaking changes

--- index.js
+++ index.js
@@ -10,8 +10,7 @@ const path = require('path')
 const PORT = process.env.PORT || 5000
 
 const bodyParser = require('body-parser')
-const form = require('express-form')
-const field = form.field;
+const { body, validationResult } = require('express-validator');
 
 var writtenNumber = require('written-number');
 writtenNumber.defaults.lang = 'fr';
@@ -24,49 +23,51 @@ express()
   .set('view engine', 'ejs')
   .get('/', (req, res) => res.render('pages/index'))
   .post('/generate_lease',
-       form(
-         field("unit").required(),
-         field("start_date").required(),
-         field("rent_amount").required(),
-         field("tenant_name").required(),
-         field("tenant_address").required(),
-         field("cotenant_name"),
-         field("cotenant_address"),
-         field("caution_name"),
-         field("caution_address"),
-         field("landlord_name").required(),
-         field("landlord_address").required()
-       ),
-       function(req, res) {
-         console.log("route generate_lease:");
-         console.log("complete body response:", req.body);
-         console.log("complete form:", req.form);
-         if (!req.form.isValid) {
-           console.log(req.form.errors);
-         } else {
-           req.form.rent_amount_written = writtenNumber(req.form.rent_amount);
-           res.render('pages/lease.ejs', req.form)
-         }
-       }
+    [
+      body('unit').notEmpty().withMessage('Unit is required'),
+      body('start_date').notEmpty().withMessage('Start date is required'),
+      body('rent_amount').notEmpty().withMessage('Rent amount is required'),
+      body('tenant_name').notEmpty().withMessage('Tenant name is required'),
+      body('tenant_address').notEmpty().withMessage('Tenant address is required'),
+      body('landlord_name').notEmpty().withMessage('Landlord name is required'),
+      body('landlord_address').notEmpty().withMessage('Landlord address is required'),
+      body('cotenant_name').optional(),
+      body('cotenant_address').optional(),
+      body('caution_name').optional(),
+      body('caution_address').optional()
+    ],
+    (req, res) => {
+      console.log("route generate_lease:");
+      console.log("complete body response:", req.body);
+      const errors = validationResult(req);
+      if (!errors.isEmpty()) {
+        console.log(errors.array());
+        return res.status(400).render('pages/index', { errors: errors.array() });
+      }
+      const formData = { ...req.body };
+      formData.rent_amount_written = writtenNumber(formData.rent_amount);
+      res.render('pages/lease.ejs', formData);
+    }
   )
   .post('/generate_information_document',
-       form(
-         field("unit").required(),
-         field("rent_amount").required(),
-         field("landlord_name").required(),
-         field("landlord_email").required(),
-         field("landlord_phone").required()
-       ),
-       function(req, res) {
-         console.log("route generate_information_document:");
-         console.log("complete body response:", req.body);
-         console.log("complete form:", req.form);
-         if (!req.form.isValid) {
-           console.log(req.form.errors);
-         } else {
-           req.form.rent_amount_written = writtenNumber(req.form.rent_amount);
-           res.render('pages/information_document.ejs', req.form)
-         }
-       }
+    [
+      body('unit').notEmpty().withMessage('Unit is required'),
+      body('rent_amount').notEmpty().withMessage('Rent amount is required'),
+      body('landlord_name').notEmpty().withMessage('Landlord name is required'),
+      body('landlord_email').notEmpty().withMessage('Landlord email is required').isEmail().withMessage('Invalid email'),
+      body('landlord_phone').notEmpty().withMessage('Landlord phone is required')
+    ],
+    (req, res) => {
+      console.log("route generate_information_document:");
+      console.log("complete body response:", req.body);
+      const errors = validationResult(req);
+      if (!errors.isEmpty()) {
+        console.log(errors.array());
+        return res.status(400).render('pages/index', { errors: errors.array() });
+      }
+      const formData = { ...req.body };
+      formData.rent_amount_written = writtenNumber(formData.rent_amount);
+      res.render('pages/information_document.ejs', formData);
+    }
   )
   .listen(PORT, () => console.log(`Listening on http://localhost:${ PORT }`))
