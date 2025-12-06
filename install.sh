d9a13aa05f5d40beb868030c0f8359bf7c5a403b
Christophe Meyer <christophe.meyer.pro@gmail.com>
2025-12-06 17:50:35 +0100

    Add Install node via npm

--- install.sh
+++ install.sh
@@ -1,5 +1,9 @@
 # spellcheck
 sudo apt-get install aspell aspell-fr
 
+# install node via nvm
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.3/install.sh | bash
+nvm install node
+
 # node deps
 npm install
